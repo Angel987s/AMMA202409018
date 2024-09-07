4187c8a9734d07886d4eb57762798bd5d3af6d00
Ángel Miguel Mena Argumedo <angelmeba4@gmail.com>
2024-09-06 20:40:54 -0600

    first commit

---
+++ src/main/java/org/esfe/AMMA202409018/Amma202409018Application.java
@@ -0,0 +1,13 @@
+package org.esfe.AMMA202409018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amma202409018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amma202409018Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/esfe/AMMA202409018/Amma202409018ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esfe.AMMA202409018;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Amma202409018ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
