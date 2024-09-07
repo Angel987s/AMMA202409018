07dbd22874a6df8e7a81d43400dc5dc6107344a6
Ángel Miguel Mena Argumedo <angelmeba4@gmail.com>
2024-09-06 20:55:49 -0600

    Agregacion del repo y interface

---
+++ src/main/java/org/esfe/AMMA202409018/repositorios/IProductoRepository.java
@@ -0,0 +1,10 @@
+package org.esfe.AMMA202409018.repositorios;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.esfe.AMMA202409018.modelos.*;
+
+
+public interface IProductoRepository extends JpaRepository<ProductAMMA, Integer>  {
+
+}
