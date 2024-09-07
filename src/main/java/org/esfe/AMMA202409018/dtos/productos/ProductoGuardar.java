9a190713ba1a30842f76d8ecbb4c64146426338d
Ángel Miguel Mena Argumedo <angelmeba4@gmail.com>
2024-09-06 20:54:13 -0600

    Creacion de los endpoints y agregacion de dependencia en pom

---
+++ src/main/java/org/esfe/AMMA202409018/dtos/productos/ProductoGuardar.java
@@ -0,0 +1,15 @@
+package org.esfe.AMMA202409018.dtos.productos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class ProductoGuardar implements Serializable {
+    private String nombreMABT;
+    private String descripcionMABT;
+    private BigDecimal precio;
+}
---
+++ src/main/java/org/esfe/AMMA202409018/dtos/productos/ProductoModificar.java
@@ -0,0 +1,16 @@
+package org.esfe.AMMA202409018.dtos.productos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class ProductoModificar implements Serializable {
+    private Integer id;
+    private String nombreMABT;
+    private String descripcionMABT;
+    private BigDecimal precio;
+}
